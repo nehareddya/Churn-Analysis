6285eeede9b90013c7b198342f0489abdb382351
nehareddya <60606249+nehareddya@users.noreply.github.com>
2021-12-03 17:11:00 +0530

    Update and rename app_flask.py to app.py
    renamed app_flask.py to app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import numpy as np
 from flask import Flask,request,jsonify,render_template
 import pickle
 
-app_flask = Flask(__name__)
+app = Flask(__name__)
 model = pickle.load(open('model_XGB.pkl','rb'))
 
 @app.route('/')

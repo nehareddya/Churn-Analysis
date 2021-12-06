df6d6762ef35a29d632e1afb8f2e9a81c6851666
nehareddya <60606249+nehareddya@users.noreply.github.com>
2021-12-06 12:52:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask,request,jsonify,render_template
 import pickle
 
 app = Flask(__name__)
-model = pickle.load(open('model_XGB.pkl','rb'))
+model = pickle.load(open('model_LR.pkl','rb'))
 
 @app.route('/')
 def home():
